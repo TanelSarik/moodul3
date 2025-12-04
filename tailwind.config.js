4c7c4e4076ea10233ad21ec3072efd5a910cdc6b
tanel sarik <tanel.sarik@tptlive.ee>
2025-12-04 16:22:31 +0200

    almost finished frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./index.html","./pood.html", "./detail.html","./kott.html", "./kruus.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
